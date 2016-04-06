d48a171892e59e90c846d5e55ee954daf26fd5c1
Esteban D.Dortta <esteban@inovacaosistemas.com.br>
2016-04-06 09:46:30 -0300

    2016-04-06 09:45:26 automatic push 0.8.48-40

--- 0.8.48/skel/chromeApp/js/yloader.js
+++ 0.8.48/skel/chromeApp/js/yloader.js
@@ -1,8 +1,8 @@
 /*********************************************
   * skel/chromeApp/js/yloader.js
-  * YeAPF 0.8.48-39 built on 2016-04-05 11:44 (-3 DST)
+  * YeAPF 0.8.48-40 built on 2016-04-06 09:45 (-3 DST)
   * Copyright (C) 2004-2016 Esteban Daniel Dortta - dortta@yahoo.com
-  * 2016-04-05 11:44:40 (-3 DST)
+  * 2016-04-06 09:45:55 (-3 DST)
   * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
   * Purpose:  Build a monolitic YeAPF script so
   *           it can be loaded at once
