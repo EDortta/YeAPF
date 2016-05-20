f33b87dcd5a4efb80d21e3645772344513e067e5
Esteban D.Dortta <dortta@yahoo.com>
2016-05-20 17:30:58 -0300

    2016-05-20 17:30:31 auto.push 0.8.48-91

--- 0.8.48/templates/bootstrap3/js/yloader.js
+++ 0.8.48/templates/bootstrap3/js/yloader.js
@@ -1,8 +1,8 @@
 /*********************************************
   * templates/bootstrap3/js/yloader.js
-  * YeAPF 0.8.48-82 built on 2016-05-20 13:57 (-3 DST)
+  * YeAPF 0.8.48-91 built on 2016-05-20 17:30 (-3 DST)
   * Copyright (C) 2004-2016 Esteban Daniel Dortta - dortta@yahoo.com
-  * 2016-05-20 13:57:29 (-3 DST)
+  * 2016-05-20 17:30:45 (-3 DST)
   * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
   * Purpose:  Build a monolitic YeAPF script so
   *           it can be loaded at once
