fd938e09c654d4787220958c46434f6171c8df70
Esteban D.Dortta <dortta@yahoo.com>
2016-11-05 11:33:28 -0200

    2016-11-05 11:32:30 0.8.51-83 Severe yAnalise() correction. '%' at the start hungs app

--- 0.8.51/skel/MoSyncApp/LocalFiles/js/ycomm-worker.js
+++ 0.8.51/skel/MoSyncApp/LocalFiles/js/ycomm-worker.js
@@ -1,8 +1,8 @@
 /*********************************************
  * skel/MoSyncApp/LocalFiles/js/ycomm-worker.js
- * YeAPF 0.8.51-82 built on 2016-11-05 09:06 (-2 DST)
+ * YeAPF 0.8.51-83 built on 2016-11-05 11:32 (-2 DST)
  * Copyright (C) 2004-2016 Esteban Daniel Dortta - dortta@yahoo.com
- * 2016-11-05 09:06:27 (-2 DST)
+ * 2016-11-05 11:32:51 (-2 DST)
  * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
 **********************************************/
 //# sourceURL=skel/MoSyncApp/LocalFiles/js/ycomm-worker.js
