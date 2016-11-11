e9f114aca282d9dc1d40e78ba60ab509e305a593
Esteban D.Dortta <dortta@yahoo.com>
2016-11-11 17:48:20 -0200

    2016-11-11 17:47:32 0.8.52-23

--- 0.8.52/skel/chromeApp/js/yloader.js
+++ 0.8.52/skel/chromeApp/js/yloader.js
@@ -1,8 +1,8 @@
 /*********************************************
   * skel/chromeApp/js/yloader.js
-  * YeAPF 0.8.52-22 built on 2016-11-11 17:46 (-2 DST)
+  * YeAPF 0.8.52-23 built on 2016-11-11 17:47 (-2 DST)
   * Copyright (C) 2004-2016 Esteban Daniel Dortta - dortta@yahoo.com
-  * 2016-11-11 17:46:08 (-2 DST)
+  * 2016-11-11 17:47:50 (-2 DST)
   * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
   * Purpose:  Build a monolitic YeAPF script so
   *           it can be loaded at once
@@ -26,7 +26,7 @@
      }
    }
  )();
- console.log("YeAPF 0.8.52-22 built on 2016-11-11 17:46 (-2 DST)");
+ console.log("YeAPF 0.8.52-23 built on 2016-11-11 17:47 (-2 DST)");
  /* START yopcontext.js */
      /***********************************************************************
       * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
--- 0.8.52/skel/workbench/index.php
+++ 0.8.52/skel/workbench/index.php
@@ -32,11 +32,12 @@
     
     foreach(glob('www/i_*') as $fileName) {
       $auxFileName=substr($fileName,4);
+      $dBody=basename(substr($auxFileName,2));
       $menu.="<div class='col-lg-6'>
                 <div class='panel panel-default'>
                   <div class='panel-heading'>
                     <button class='btn btn-default'><i class='fa fa-puzzle-piece'></i></button>
-                    <a class='btn btn-danger' href='?dBody=$auxFileName'><i class='fa fa-close'></i></a>
+                    <a class='btn btn-danger' href='?dBody=$dBody'><i class='fa fa-close'></i></a>
                   </div>
                   <div class='panel-body'>
                     <a href='$fileName'>$auxFileName</a>
