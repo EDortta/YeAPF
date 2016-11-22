7f9e7473d8f8011ff1d155e26e8c9d6de34d2926
Esteban D.Dortta <dortta@yahoo.com>
2016-11-22 10:59:41 -0200

    2016-11-22 10:26:53 0.8.52-73

--- 0.8.52/skel/chromeApp/js/ysandboxifc.js
+++ 0.8.52/skel/chromeApp/js/ysandboxifc.js
@@ -1,8 +1,8 @@
 /*********************************************
  * skel/chromeApp/js/ysandboxifc.js
- * YeAPF 0.8.52-71 built on 2016-11-22 09:01 (-2 DST)
+ * YeAPF 0.8.52-73 built on 2016-11-22 10:27 (-2 DST)
  * Copyright (C) 2004-2016 Esteban Daniel Dortta - dortta@yahoo.com
- * 2016-11-22 09:01:08 (-2 DST)
+ * 2016-11-22 10:27:27 (-2 DST)
  * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
  * By security reasons, sometimes you cannot acces
  * your restful interface from your webapp, so you need a bridge.
