bfd41eae8ba1cd97a4b8d1ed7880c68840e1016b
Esteban D.Dortta <dortta@yahoo.com>
2017-02-24 09:33:43 -0300

    2017-02-24 09:28:51 0.8.54-45 i18n improvements

--- 0.8.54/skel/workbench/sse.php
+++ 0.8.54/skel/workbench/sse.php
@@ -1,9 +1,9 @@
 <?php
 /*
     skel/workbench/sse.php
-    YeAPF 0.8.54-44 built on 2017-02-23 17:20 (-3 DST)
+    YeAPF 0.8.54-45 built on 2017-02-24 09:29 (-3 DST)
     Copyright (C) 2004-2017 Esteban Daniel Dortta - dortta@yahoo.com
-    2017-02-23 17:20:11 (-3 DST)
+    2017-02-24 09:29:15 (-3 DST)
 
     skel/webApp / sse.php
     This file cannot be modified within skel/webApp
--- 0.8.54/skel/workbench/www/js/yifc.js
+++ 0.8.54/skel/workbench/www/js/yifc.js
@@ -1,8 +1,8 @@
 /*********************************************
  * skel/workbench/www/js/yifc.js
- * YeAPF 0.8.54-44 built on 2017-02-23 17:20 (-3 DST)
+ * YeAPF 0.8.54-45 built on 2017-02-24 09:29 (-3 DST)
  * Copyright (C) 2004-2017 Esteban Daniel Dortta - dortta@yahoo.com
- * 2017-02-23 17:20:12 (-3 DST)
+ * 2017-02-24 09:29:15 (-3 DST)
  * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
  * This object allows to communicate with ysandboxifc.js
  * and runs outside the sandbox.
