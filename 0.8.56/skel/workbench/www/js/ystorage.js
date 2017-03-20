f7013ba53be04859a9cd99ee1f9d4de11d711956
Esteban D.Dortta <dortta@yahoo.com>
2017-03-20 19:33:15 -0300

    2017-03-20 19:32:26 0.8.56-20 introduction of ycheckdistsource. minor bug correction of ydistbuilder

--- 0.8.56/skel/workbench/www/js/ystorage.js
+++ 0.8.56/skel/workbench/www/js/ystorage.js
@@ -1,8 +1,8 @@
 /*********************************************
  * skel/workbench/www/js/ystorage.js
- * YeAPF 0.8.56-19 built on 2017-03-20 19:28 (-3 DST)
+ * YeAPF 0.8.56-20 built on 2017-03-20 19:32 (-3 DST)
  * Copyright (C) 2004-2017 Esteban Daniel Dortta - dortta@yahoo.com
- * 2017-03-20 19:28:52 (-3 DST)
+ * 2017-03-20 19:32:41 (-3 DST)
  * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
  * yServerWatcherObj and yInfoObj introduced in 2016-08-22 0.8.50-0
  *********************************************/
