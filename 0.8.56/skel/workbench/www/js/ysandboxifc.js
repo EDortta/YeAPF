2077da77ee8b4085c943d76beffec42d4c1555ae
Esteban D.Dortta <dortta@yahoo.com>
2017-03-24 10:36:39 -0300

    2017-03-24 10:35:43 0.8.56-34 workbench improvements

--- 0.8.56/skel/workbench/www/js/ysandboxifc.js
+++ 0.8.56/skel/workbench/www/js/ysandboxifc.js
@@ -1,8 +1,8 @@
 /*********************************************
  * skel/workbench/www/js/ysandboxifc.js
- * YeAPF 0.8.56-33 built on 2017-03-24 10:34 (-3 DST)
+ * YeAPF 0.8.56-34 built on 2017-03-24 10:36 (-3 DST)
  * Copyright (C) 2004-2017 Esteban Daniel Dortta - dortta@yahoo.com
- * 2017-03-24 10:34:22 (-3 DST)
+ * 2017-03-24 10:36:02 (-3 DST)
  * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
  * By security reasons, sometimes you cannot acces
  * your restful interface from your webapp, so you need a bridge.
