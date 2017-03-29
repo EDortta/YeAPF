10d9fc0caa5db2c038df02d884bd9c7c819f2b5c
Esteban D.Dortta <dortta@yahoo.com>
2017-03-29 17:05:03 -0300

    2017-03-29 17:02:08 0.8.56-75 workbench improvements

--- 0.8.56/skel/workbench/www/js/yloader.js
+++ 0.8.56/skel/workbench/www/js/yloader.js
@@ -1,8 +1,8 @@
 /*********************************************
   * skel/workbench/www/js/yloader.js
-  * YeAPF 0.8.56-74 built on 2017-03-29 06:19 (-3 DST)
+  * YeAPF 0.8.56-75 built on 2017-03-29 17:02 (-3 DST)
   * Copyright (C) 2004-2017 Esteban Daniel Dortta - dortta@yahoo.com
-  * 2017-03-29 06:19:39 (-3 DST)
+  * 2017-03-29 17:02:31 (-3 DST)
   * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
   * Purpose:  Build a monolitic YeAPF script so
   *           it can be loaded at once
@@ -26,7 +26,7 @@
      }
    }
  )();
- console.log("YeAPF 0.8.56-74 built on 2017-03-29 06:19 (-3 DST)");
+ console.log("YeAPF 0.8.56-75 built on 2017-03-29 17:02 (-3 DST)");
  /* START yopcontext.js */
      /***********************************************************************
       * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
