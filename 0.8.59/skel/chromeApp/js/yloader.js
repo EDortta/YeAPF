88ba56d7a81cf2bbed626e60e9b0dd8cd5cc399d
Esteban D.Dortta <dortta@yahoo.com>
2017-08-29 07:15:28 -0300

    2017-08-29 07:13:46 automatic push 0.8.59-43

--- 0.8.59/skel/chromeApp/js/yloader.js
+++ 0.8.59/skel/chromeApp/js/yloader.js
@@ -1,8 +1,8 @@
 /*********************************************
   * skel/chromeApp/js/yloader.js
-  * YeAPF 0.8.59-41 built on 2017-08-28 20:59 (-3 DST)
+  * YeAPF 0.8.59-43 built on 2017-08-29 07:14 (-3 DST)
   * Copyright (C) 2004-2017 Esteban Daniel Dortta - dortta@yahoo.com
-  * 2017-08-28 20:59:22 (-3 DST)
+  * 2017-08-29 07:14:07 (-3 DST)
   * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
   * Purpose:  Build a monolitic YeAPF script so
   *           it can be loaded at once
@@ -26,7 +26,7 @@
      }
    }
  )();
- console.log("YeAPF 0.8.59-41 built on 2017-08-28 20:59 (-3 DST)");
+ console.log("YeAPF 0.8.59-43 built on 2017-08-29 07:14 (-3 DST)");
  /* START yopcontext.js */
      /***********************************************************************
       * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
--- 0.8.59/skel/webApp/index.php
+++ 0.8.59/skel/webApp/index.php
@@ -1,9 +1,9 @@
 <?php
 /*
     skel/webApp/index.php
-    YeAPF 0.8.59-41 built on 2017-08-28 20:59 (-3 DST)
+    YeAPF 0.8.59-43 built on 2017-08-29 07:14 (-3 DST)
     Copyright (C) 2004-2017 Esteban Daniel Dortta - dortta@yahoo.com
-    2017-08-28 19:44:55 (-3 DST)
+    2017-08-29 06:53:35 (-3 DST)
 
     skel/webApp / index.php
     This file cannot be modified within skel/webApp
@@ -61,7 +61,7 @@
     die("<div  style='border-color: #CB0000; background: #FFC0CB; width: 520px; margin: 8px; padding: 32px; border-style: solid; border-width: 2px; padding: 16px; border-radius:4px; font-family: arial; font-size: 12px'>
              <b>yeapf.php cannot be found<br>
              <a href='configure.php?devSession=$devSession'>Click here to configure</a></b><br><br>
-             YeAPF 0.8.59-41 built on 2017-08-28 20:59 (-3 DST)<br>
+             YeAPF 0.8.59-43 built on 2017-08-29 07:14 (-3 DST)<br>
              Copyright (C) 2004-2017 Esteban Daniel Dortta - dortta@yahoo.com
         </div>");
   }
@@ -162,6 +162,8 @@
           $aBody=bestName($logonForm);
           if ($aBody=='')
             die("'$logonForm' not found");
+        } else {
+          die("$lastError");
         }
         break;
 
