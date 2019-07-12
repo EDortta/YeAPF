fa0f4045536d6095f95411fc9cfc6a0ce2efc93c
Esteban D.Dortta <esteban@inovacaosistemas.com.br>
2019-07-12 15:24:15 -0300

    Improvements in ydocview in order to allow conversion of absolute URLs 0.8.63-113 2019-07-12 15:22:49

--- 0.8.63/skel/electron/js/yloader.js
+++ 0.8.63/skel/electron/js/yloader.js
@@ -1,8 +1,8 @@
 /*********************************************
   * skel/electron/js/yloader.js
-  * YeAPF 0.8.63-110 built on 2019-07-11 14:44 (-3 DST)
+  * YeAPF 0.8.63-113 built on 2019-07-12 15:23 (-3 DST)
   * Copyright (C) 2004-2019 Esteban Daniel Dortta - dortta@yahoo.com - MIT License
-  * 2019-07-11 14:44:49 (-3 DST)
+  * 2019-07-12 15:23:24 (-3 DST)
   * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
   * Purpose:  Build a monolitic YeAPF script so
   *           it can be loaded at once
@@ -26,7 +26,7 @@
      }
    }
  )();
- console.log("YeAPF 0.8.63-110 built on 2019-07-11 14:44 (-3 DST)");
+ console.log("YeAPF 0.8.63-113 built on 2019-07-12 15:23 (-3 DST)");
  /* START yopcontext.js */
      /***********************************************************************
       * First Version (C) 2014 - esteban daniel dortta - dortta@yahoo.com
@@ -9683,9 +9683,9 @@
  /* START yinterface.js */
      /*
          skel/electron/js/yloader.js
-         YeAPF 0.8.63-110 built on 2019-07-11 14:44 (-3 DST)
+         YeAPF 0.8.63-113 built on 2019-07-12 15:23 (-3 DST)
          Copyright (C) 2004-2019 Esteban Daniel Dortta - dortta@yahoo.com - MIT License
-         2019-07-11 14:44:49 (-3 DST)
+         2019-07-12 15:23:24 (-3 DST)
      */
      
      var yInterfaceObj = function() {
--- 0.8.63/skel/webApp/templates/milligram-1.3/rest.php
+++ 0.8.63/skel/webApp/templates/milligram-1.3/rest.php
@@ -1,9 +1,9 @@
 <?php
 /*
     skel/webApp/templates/milligram-1.3/rest.php
-    YeAPF 0.8.63-110 built on 2019-07-11 14:44 (-3 DST)
+    YeAPF 0.8.63-113 built on 2019-07-12 15:23 (-3 DST)
     Copyright (C) 2004-2019 Esteban Daniel Dortta - dortta@yahoo.com - MIT License
-    2019-07-11 14:44:49 (-3 DST)
+    2019-07-12 15:23:24 (-3 DST)
 
     skel/webApp / rest.php
     This file cannot be modified within skel/webApp
